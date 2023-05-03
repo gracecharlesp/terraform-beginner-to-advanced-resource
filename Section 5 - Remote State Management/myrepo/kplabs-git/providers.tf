7fad00f1bf3186368c30ef470d18c779c0b6f9be
gracecharlesp <104120618+gracecharlesp@users.noreply.github.com>
2023-05-03 13:37:53 -0500

    Update providers.tf

--- Section 5 - Remote State Management/myrepo/kplabs-git/providers.tf
+++ Section 5 - Remote State Management/myrepo/kplabs-git/providers.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = ""
-  secret_key = ""
+ access_key = "AKIA3FJNM7GU4DL22S77"
+   secret_key = "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"
 }
